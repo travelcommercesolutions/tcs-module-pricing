031ab99485d233a011d3731031c0c9a6e52a03ba
vipul.patel <vipul.patel@accelya.com>
2020-12-28 15:34:35 +0530

    Increase module version
    Increase module version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("TravelCommerce Pricing Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2019")]
 
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.2")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
